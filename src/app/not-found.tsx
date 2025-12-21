8791e232ae2004fdea105ad291174ca4d5ad644b
Guil. Sperb Machado <guil@axlabs.com>
2025-12-21 00:43:04 -0300

    Remove favicon.ico and add new icon.tsx component for dynamic image generation. Update multiple files to set runtime to 'edge' for improved performance.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import { Button } from '@/ui/shadcn/button';
 
+export const runtime = 'edge';
+
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center">
